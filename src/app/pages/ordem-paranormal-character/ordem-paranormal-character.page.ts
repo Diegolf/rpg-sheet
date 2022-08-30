e4a60d065d1fe09949eae336d7ab31ce9022e812
Diego Ferreira <diego.ferreira@trybu.com.br>
2022-08-30 19:20:23 -0300

    Ordem Paranormal Character Page

---
+++ src/app/pages/ordem-paranormal-character/ordem-paranormal-character.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordem-paranormal-character',
+  templateUrl: './ordem-paranormal-character.page.html',
+  styleUrls: ['./ordem-paranormal-character.page.scss'],
+})
+export class OrdemParanormalCharacterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
